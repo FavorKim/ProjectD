cd5f0179388f6b4b0491f153bc45f2ba41eb4a8e
김하랑 <kimharangs@gmail.com>
2024-07-06 15:12:41 +0900

    [feat] 퍽 테이블 DB연동부 추가
    근데 MySQL.dll이 자꾸 문제가 생겨서 빌드불가능.
    또한, 비밀번호 및 권한 설정 떄문에 접속이 안되는 문제 있음

--- Assets/Scripts/UI/Perk/PerkDataManager.cs
+++ Assets/Scripts/UI/Perk/PerkDataManager.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Xml.Linq;
 using UnityEngine;
+using MySql.Data.MySqlClient;
 
 
 
@@ -10,10 +9,10 @@ public class PerkDataManager : SingletonMono<PerkDataManager>
 {
     public Dictionary<string, PerkData> LoadedPerkList;
 
-    public readonly string rootPath = "C:/Users/KGA/Desktop/ProjectD_Data";
+    //public readonly string rootPath = "C:/Users/KGA/Desktop/ProjectD_Data";
 
+    public string connectionString = "server=localhost;database=projectd;user=root;password=3010";
     
-    //[TODO 상대경로화] public readonly string perkTablePath = Directory.GetParent(Application.dataPath).FullName;
 
     private void Awake()
     {
@@ -21,12 +20,44 @@ public class PerkDataManager : SingletonMono<PerkDataManager>
         DontDestroyOnLoad(gameObject);
         LoadAllPerkData();
     }
+    void LoadAllPerkData()
+    {
+        LoadedPerkList = new Dictionary<string, PerkData>();
 
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string query = "SELECT * FROM perktable";
+            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+            {
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var tempPerk = new PerkData();
+                        tempPerk.Owner = reader["Owner"].ToString();
+                        tempPerk.PerkName = reader["PerkName"].ToString();
+                        tempPerk.IconImg = Resources.Load<Sprite>($"Perks/{tempPerk.Owner}/{tempPerk.PerkName}");
+                        tempPerk.EffectTarget = (HandleValue)Enum.Parse(typeof(HandleValue), reader["EffectTarget"].ToString());
+                        tempPerk.ValuePercentage = float.Parse(reader["ValuePercentage"].ToString());
+                        tempPerk.Duration = float.Parse(reader["Duration"].ToString());
+                        tempPerk.CoolTime = float.Parse(reader["CoolTime"].ToString());
+                        tempPerk.PerkType = (PerkType)Enum.Parse(typeof(PerkType), reader["PerkType"].ToString());
+                        var desc = reader["Description"].ToString();
+                        tempPerk.Description = string.Format(desc, tempPerk.ValuePercentage, tempPerk.CoolTime, tempPerk.Duration);
+
+                        LoadedPerkList.Add(tempPerk.PerkName, tempPerk);
+                    }
+                }
+            }
+        }
+    }
+    /*
     void LoadAllPerkData()
     {
         LoadedPerkList = new Dictionary<string, PerkData>();
         XDocument doc = XDocument.Load($"{rootPath}/PerkTable.xml");
-        //XDocument doc = XDocument.Load($"{perkTablePath}/PerkTable.xml");
         var dataElements = doc.Descendants("data");
 
         foreach ( var data in dataElements )
@@ -46,7 +77,7 @@ public class PerkDataManager : SingletonMono<PerkDataManager>
             LoadedPerkList.Add(tempPerk.PerkName, tempPerk);
         }
     }
-
+    */
     public PerkData GetPerkByName(string perkName)
     {
         var perk = LoadedPerkList[perkName];
